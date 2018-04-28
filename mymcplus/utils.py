943b6903b469dd984471fa8250eece30ab0a1c20
Florian Märkl <info@florianmaerkl.de>
2018-04-28 18:24:38 +0200

    Add missing Header Comments

--- mymcplus/utils.py
+++ mymcplus/utils.py
@@ -1,3 +1,19 @@
+#
+# This file is part of mymc+, based on mymc by Ross Ridge.
+#
+# mymc+ is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# mymc+ is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with mymc+.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 def zero_terminate(s):
     """Truncate a string at the first NUL ('\0') character, if any."""
